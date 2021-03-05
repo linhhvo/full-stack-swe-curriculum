dd8e8c033569e966e4a14529622b80fee2787f56
Linh <lhvo2112@gmail.com>
2021-03-05 13:26:59 -0500

    Change folder names

--- advanced_methods_exercise/05_max_inject.rb
+++
@@ -1,9 +0,0 @@
-# Write a method, max_inject(arr), that accepts any amount of numbers arguments and returns
-# the largest number. Solve this using the built-in inject.
-
-def max_inject(*arr)
-  arr.inject {|max, current| [max, current].max}
-end
-
-p max_inject(1, -4, 0, 7, 5)  # => 7
-p max_inject(30, 28, 18)      # => 30
